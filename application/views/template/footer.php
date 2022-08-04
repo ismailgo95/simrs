8f2427626a38f7ce91574b19a0c849d52f47cbcb
ismailgo95 <mail.kerja1995@gmail.com>
2022-08-04 09:02:13 +0700

    Fix console

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -16,12 +16,6 @@
 
 <!-- jQuery -->
 <script src="<?= base_url(); ?>assets/plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="<?= base_url(); ?>assets/plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
 <!-- Bootstrap 4 -->
 <script src="<?= base_url(); ?>assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 
